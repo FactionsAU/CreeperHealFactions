846e3e968d42f702d22ab1086a3d763734c15036
Ryan Leach <rleach@rleach.id.au>
2014-04-22 16:09:32 +0930

    More comments added with beautiful ascii depiction.

--- src/main/java/au/id/rleach/creeperhealfactions/timewarp/Overshooter.java
+++ src/main/java/au/id/rleach/creeperhealfactions/timewarp/Overshooter.java
@@ -17,43 +17,80 @@ public class Overshooter {
 
     private PlayerTime pt;
 
+    /** Overshooter is a "simple" class to make an animation between 2 values.
+    * it starts off with a speed in the opposite direction to the goal, but accelerates
+    * towards the goal until the goal is passed, at which stage the direction of 
+    * acceleration is reversed. 
+    * Speed is also clamped to a maximum value.
+    * When the animation is complete, PlayerTime.stop is called to remove it from the 
+    * active animations list.
+    * 
+    * This also works in both the positive and negative directions just fine.
+    * <pre>
+    * - y-axis position. 
+    *| 
+    *|             *** <-overshoot             >
+    *|            *   * 
+    *| ----------*-----*Goal
+    *|          *
+    *| *-------*--Start
+    *|  *     *
+    *|   *   *
+    *|    *** <-undershoot                     >
+    * \----------------------- Time
+    * </pre>
+    * 
+    * http://puu.sh/8iBIe.jpg paint drawing, if it is still up.
+    **/
     Overshooter(long curTime, long goal, PlayerTime pt) {
         this.pt = pt;
         start(curTime, goal);
     }
 
+    //initialize the animation starting position/velocity/acceleration
     private void start(long start, long end) {
         position = start;
         goal = end;
+        //Always towards the goal from the position
         acceleration = Long.signum(goal - position*5);
         oldAcceleration = acceleration;
         accelerationChanged = false;
+        //initial speed, in opposite direction of goal for nice undershoot.
         speed = acceleration * -overshoot;
     }
 
+    //get the next position of the animation
     public long next() {
+        //Always towards the goal from the position
         acceleration = Long.signum(goal - position);
+        //Restrict max speed for smooth animation
         speed = clamp(speed + acceleration, -overshoot, overshoot);
+        //if goal reached, notify parent
         if (testGoal()) {
             pt.stop();
             return goal;
         }
+        //otherwise keep on truckin'
         position = position + speed;
         return position;
     }
 
+    //whether the goal has been reached (after the overshoot)
     boolean testGoal() {
+        //remember if we overshoot
         if (acceleration != oldAcceleration) {
             accelerationChanged = true;
         }
+        //if overshot previously and goal reached.
         return accelerationChanged && acceleration == oldAcceleration;
 
     }
-
+    //clamps the value to the specified range.
     int clamp(int val, int min, int max) {
         return Math.max(min, Math.min(max, val));
     }
 
+    //resets a new goal whilst maintaining speed/momentum
     void setGoal(long goal) {
         start(this.position, goal);
         this.speed = speed;
